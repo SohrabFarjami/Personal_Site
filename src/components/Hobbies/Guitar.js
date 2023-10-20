bc7a1fd72f40f9baa9dcfda8f295e4adaaff8de7
SohrabFarjami <111026287+SohrabFarjami@users.noreply.github.com>
2023-10-20 15:31:21 +0300

    Revert "fixed performance issues"
    This reverts commit a2395b5843ee6cdb0e6b307ba0f0f1780d20b293.

--- src/components/Hobbies/Guitar.js
+++ src/components/Hobbies/Guitar.js
@@ -3,13 +3,14 @@ import CSS from './Guitar.module.css'
 import Box from './components/Box'
 import { Link } from 'react-router-dom'
 import Clock from './components/Clock/Clock'
+import Typerwriter from './components/Typewriter/Typerwritertext'
 
 export const Guitar = () => {
-
+  const sentence = 'Im still building the site '
   return (
       <div>
         <section id={CSS.peepee}    >
-          <div className={CSS.bigtext}> <h>IM A SLEEK LITTLE BOY OHH MAH GAWDDD. WHAT DA WHAT DA DOG DOING THO???ss?</h></div>
+          <div className={CSS.bigtext}> <h>IM A SLEEK LITTLE BOY OHH MAH GAWDDD. WHAT DA WHAT DA DOG DOING THO????</h></div>
           </section>
         <section id = {CSS.sectiontwo}>
              <Clock/>
