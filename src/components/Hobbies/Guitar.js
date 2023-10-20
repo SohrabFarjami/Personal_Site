05eb6a23513c55bdc16ad2fda3425d80d11c9f6b
SohrabFarjami <111026287+SohrabFarjami@users.noreply.github.com>
2023-10-20 15:28:21 +0300

    test

--- src/components/Hobbies/Guitar.js
+++ src/components/Hobbies/Guitar.js
@@ -9,7 +9,7 @@ export const Guitar = () => {
   return (
       <div>
         <section id={CSS.peepee}    >
-          <div className={CSS.bigtext}> <h>IM A SLEEK LITTLE BOY OHH MAH GAWDDD. WHAT DA WHAT DA DOG DOING THO????</h></div>
+          <div className={CSS.bigtext}> <h>IM A SLEEK LITTLE BOY OHH MAH GAWDDD. WHAT DA WHAT DA DOG DOING THO???ss?</h></div>
           </section>
         <section id = {CSS.sectiontwo}>
              <Clock/>
